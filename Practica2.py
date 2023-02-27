0bf61543a5148fdee977d94c19ce37c1b824960f
ramonzeta <126364694+ramonzeta@users.noreply.github.com>
2023-02-27 17:09:02 -0600

    cambio mensaje

--- Practica2.py
+++ Practica2.py
@@ -5,4 +5,4 @@ numero= int(input("Por favor introduce un numero entero: "))
 if numero % 2 == 0:
     print("El numero ", numero, " es par.")
 elif numero % 2 == 1:
-    print("El numero ", numero, " es impar.")
+    print("El numero ", numero, " es impar chido.")
